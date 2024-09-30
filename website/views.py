1b55ebdca4736ad1c75b9dde3957f8292fb55e0a
Heriberto Forero Correa <hforeroc@unal.edu.co>
2024-09-29 21:45:48 -0500

    Individual Records

--- website/views.py
+++ website/views.py
@@ -46,3 +46,12 @@ def register_user(request):
 		return render(request, 'register.html', {'form':form})
 
 	return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look Up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You Must Be Logged In To Do That...")
+        return redirect('home')
